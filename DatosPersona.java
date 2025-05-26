ecc5fd667ecc86a4f919f75543dc2eade8453c94
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-05-26 09:42:47 -0600

    Clase DatosPersona creada con sus métodos

--- DatosPersona.java
+++ DatosPersona.java
@@ -1,3 +1,54 @@
 public class DatosPersona {
 
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public DatosPersona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    //Métodos
+    public void concentrarse() {
+        System.out.println("Concentrándose en ...");
+    }
+
+    public void viajar() {
+        System.out.println("Viajando");
+    }
 }
