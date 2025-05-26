03338d94317418e60daaee6c4afbafe33516f290
Emely Gabriela Nájera Serrano <23emelynajera@gmail.com>
2025-05-26 10:20:25 -0600

    Clase Masajista terminada.

--- src/Masajista.java
+++ src/Masajista.java
@@ -1,4 +1,47 @@
 package src;
-public class Masajista {
+public class Masajista extends DatosPersona{
+private String titulacion;
+private int aniosExperiencia;
+
+public Masajista() {
+}
+
+ public Masajista(String nombre, String apellido, int edad,
+            String titulacion, int aniosExperiencia) {
+        super(nombre, apellido, edad);
+        this.titulacion = titulacion;
+        this.aniosExperiencia= aniosExperiencia;
+    }
+
+ public String getTitulacion() {
+    return titulacion;
+ }
+
+ public void setTitulacion(String titulacion) {
+    this.titulacion = titulacion;
+ }
+
+ public int getAniosExperiencia() {
+    return aniosExperiencia;
+ }
+
+ public void setAniosExperiencia(int aniosExperiencia) {
+    this.aniosExperiencia = aniosExperiencia;
+ }
+
+    // Métodos específicos de Masajista
+    public void darMasaje() {
+        System.out.println("Dando un masaje");
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println("Concentrándose hacer un masaje");
+    }
+
+    @Override
+    public void viajar() {
+        System.out.println("Viajando para dar un masaje");
+    }
 
 }
